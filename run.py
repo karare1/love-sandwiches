825d7e5cf1dcbbe5079fa4775c9624d510549a55
karare1 <defi20@yahoo.co.uk>
2023-02-14 23:03:53 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -5,49 +5,49 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 # sales = SHEET.worksheet('sales')
+
 # data = sales.get_all_values()
+
 # print(data)
 
-
 def get_sales_data():
-
     """
-    Get sales input from the user
+    Get sales figures input from the user.
     """
     while True:
 
-        print('Please enter sales data from the last market.')
-        print('Data should be six number separated by commas.')
-        print('Example: 10, 20, 30, 40, 50, 60\n')
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input('Enter your data here:')
-        # print(f'The data provided is {data_str}.')
-        sales_data = data_str.split(',')
-        # print(data_sales)
+        data_str = input("Enter your data here: ")
+        # print(f"The data provided is {data_str}")
+        sales_data = data_str.split(",")
+
         if validate_data(sales_data):
             print('Data is valid!')
             break
 
-    return sales_data
+    retun sales_data
+
 
 
 def validate_data(values):
-
+    # print(values)
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
-    The loop will repeatedly request data, until it is valid.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -56,23 +56,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-
         return False
+   
     return True
 
-
-def update_sales_worksheet(data):
-
-    """
-    Update sales worksheet, add new row with the list data provided.
-    """
-    print('Updating sales worksheet...')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
 data = get_sales_data()
-# print(data)
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
